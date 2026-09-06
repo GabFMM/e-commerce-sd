75eb08e6382686dca581b4c36ce229a68da12044
GabFMM <gabrielfmmachado@gmail.com>
2026-09-06 19:22:33 -0300

    feat: modifica o comportamento do setup pelo argumento da linha de comando

--- setup/main/main.go
+++ setup/main/main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"os"
 	"log"
 	"e-commerce-sd/setup"
 )
 
 func main() {
+	args := os.Args
+
+	if len(os.Args) == 0 {
+		log.Panic("[ERRO] Esperava 1 argumento na linha de comando, mas obteve 0")
+		return
+	}
+
 	setup.ConfigurarFilas()
-	setup.IniciarBanco()
+
+	switch args[1] {
+	case "iniciar":
+		setup.IniciarBanco()
+	case "encerrar":
+		setup.EncerrarBanco()
+	}
+
 	log.Println("[LOG] Setup concluído com sucesso.")
 }
